f3f822091cc1ec691a9aab53aeaf05186a5b53ca
HK-MBURU <kiarii.mburu20@students.dkut.ac.ke>
2024-10-24 09:36:40 +0300

    third commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mx-auto px-4 py-6" x-data="{ showModal: false, projectId: null }">
-    <h1 class="text-3xl font-bold mb-6 text-center">Project Management</h1>
+    
 
     <div class="overflow-x-auto">
         <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
